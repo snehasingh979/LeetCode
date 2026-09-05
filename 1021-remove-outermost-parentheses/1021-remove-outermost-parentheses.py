c4c21a7421cabd88873ee2f9fc6d75155e178fa5
sneha singh <snehasingh20753@bbdu.ac.in>
2026-09-05 14:44:19 +0530

    Time: 7 ms (49.11%), Space: 12.6 MB (34.8%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        result = ""
+        count = 0
+
+        for ch in s:
+            if ch == "(":
+                count += 1
+                if count > 1:
+                    result += ch
+            else:
+                count -= 1
+                if count > 0:
+                    result += ch
+
+        return result
